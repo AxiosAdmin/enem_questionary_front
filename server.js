0bbdcdd0d444bbc11b9a8821a8f9112d73e27a1f
Pedro Vieira <pedro515vieira@gmail.com>
2026-05-21 21:22:55 -0300

    [feature] - Added deploy setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/env-config.js", (req, res) => {
+  res.type("application/javascript");
+  res.send(
+    `window.__APP_CONFIG__ = ${JSON.stringify({
+      REACT_APP_BASE_API_URL: process.env.REACT_APP_BASE_API_URL || "",
+    })};`,
+  );
+});
+
+// Serve the React build output.
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/{*path}", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
